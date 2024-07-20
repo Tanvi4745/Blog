2c0989f50b248c1e465f361098419ba9446c1a97
Tanvi4745 <147968745+Tanvi4745@users.noreply.github.com>
2024-07-20 14:56:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 4048;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
